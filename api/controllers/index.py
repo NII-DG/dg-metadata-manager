187596f4b6070fc0c1884ea0d147e0999e76ea53
Dai Tsukioka <dai.tsukioka@ivis.co.jp>
2022-12-19 09:26:48 +0900

    不要ファイルの削除

--- api/controllers/index.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint
-
-index_page = Blueprint("index_page", __name__)
-
-
-@index_page.route("/")
-def index():
-
-    return "hello"
--- www.py
+++
@@ -1,8 +0,0 @@
-from application import app
-from api.controllers.index import index_page
-
-from flask_debugtoolbar import DebugToolbarExtension
-toolbar = DebugToolbarExtension(app)
-
-
-app.register_blueprint(index_page, url_prefix="/")
