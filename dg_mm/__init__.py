0aa15cb8ebbba0b77311934cf285dce495a28a6a
ivis-saito <makio.saito@ivis.co.jp>
2024-10-04 11:48:05 +0900

    ログの設定&importの設定

--- dg_mm/__init__.py
+++ dg_mm/__init__.py
@@ -1,4 +1,9 @@
-from dg_mm import *
+from logging import getLogger, NullHandler
+
+from dg_mm.models.metadata_manager import MetadataManger
+
+logger = getLogger(__name__)
+logger.addHandler(NullHandler())
 
 
 __version__ = '1.0.0'
