fecfa6c5099114555a27fba712d49c70346c247f
Kihiro.Ashino <kihiro.ashino@ivis.co.jp>
2024-10-08 15:49:45 +0900

    MetadataManager, GrdmAccess, mainの実装

--- dg_mm/exceptions.py
+++ dg_mm/exceptions.py
@@ -2,15 +2,58 @@
 
 class NotFoundMappingDefinitionError(Exception):
     """マッピング定義ファイルが見つからないエラーのクラスです。"""
+    pass
 
 class MetadataTypeError(Exception):
     """データの型が間違っているエラーのクラスです。"""
+    pass
+
 
 class NotFoundKeyError(Exception):
     """キーが見つからない場合のエラークラスです。"""
+    pass
+
 
 class MappingDefinitionError(Exception):
     """マッピング定義に誤りがある場合のエラークラスです。"""
+    pass
+
 
 class DataFormatError(Exception):
     """データの形式に問題がある場合のエラーです。"""
+    pass
+
+
+class MetadatamanagerError(Exception):
+    """MetadataManagerのエラー"""
+    pass
+
+
+class APIError(MetadatamanagerError):
+    """APIのエラー"""
+    pass
+
+
+class UnauthorizedError(MetadatamanagerError):
+    """未認証のエラー"""
+    pass
+
+
+class AccessDeniedError(MetadatamanagerError):
+    """アクセス権限がない場合のエラー"""
+    pass
+
+
+class InvalidTokenError(MetadatamanagerError):
+    """トークン不正の場合のエラー"""
+    pass
+
+
+class InvalidProjectError(MetadatamanagerError):
+    """プロジェクト不正の場合のエラー"""
+    pass
+
+
+class StorageNameError(MetadatamanagerError):
+    """対応していないストレージを指定した場合のエラー"""
+    pass
