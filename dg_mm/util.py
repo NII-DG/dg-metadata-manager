7385950b5dc95b4f1ce9efcd002c18db9552f9d3
ivis-saito <makio.saito@ivis.co.jp>
2024-10-18 09:55:12 +0900

    パッケージ内にpythonのファイル以外を含める対応

---
+++ dg_mm/util.py
@@ -0,0 +1,53 @@
+import json
+from configparser import ConfigParser
+from pathlib import Path
+
+
+class ReadPackageFile():
+    """パッケージ内のファイルの読み込み処理をまとめたクラスです。"""
+
+    @classmethod
+    def read_json(cls, relative_path: str) -> dict:
+        """JSONファイルを読み込むメソッドです。
+
+        Args:
+            relative_path (str): ファイルパス(dg_mmフォルダからの相対パス)
+
+        Returns:
+            dict: jsonから変換したPythonオブジェクト
+        """
+
+        file_path = cls._get_absolute_path(relative_path)
+        with open(file_path) as f:
+            return json.load(f)
+
+    @classmethod
+    def read_ini(cls, relative_path: str) -> ConfigParser:
+        """iniファイルを読み込むメソッドです。
+
+        Args:
+            relative_path (str): ファイルパス(dg_mmフォルダからの相対パス)
+
+        Returns:
+            ConfigParser: 設定ファイルのパーサー
+        """
+
+        file_path = cls._get_absolute_path(relative_path)
+        ini_file = ConfigParser()
+        ini_file.read(file_path)
+        return ini_file
+
+    @classmethod
+    def _get_absolute_path(cls, relative_path: str):
+        """ファイルの絶対パスを取得するメソッドです。
+
+        Args:
+            relative_path (str): ファイルパス(dg_mmフォルダからの相対パス)
+
+        Returns:
+            str: 絶対パス
+        """
+
+        package_path = Path(__file__).resolve().parent
+        file_path = package_path.joinpath(relative_path)
+        return file_path
